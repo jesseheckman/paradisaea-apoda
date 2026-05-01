eef6ea070a04421b6f4749195368cc620a26404a
Jesse Heckman <jesse.heckman@gmail.com>
2026-05-01 14:55:19 +0200

    up

--- src/apoda/__init__.py
+++ src/apoda/__init__.py
@@ -1,10 +1,13 @@
 from importlib.metadata import version
+
 from .schemas import PlotSchema
 from .themes import DEFAULT, MINIMAL
+from .style  import apply, helper_hline, helper_vline
 
-__all__ = ["PlotSchema", "DEFAULT", "MINIMAL"]
+__all__ = ["PlotSchema", "DEFAULT", "MINIMAL", "apply", "helper_hline", "helper_vline"]
+
 __version__ = version("paradisaea-apoda")
+
 
 def pkaa() -> str:
     return "Pkaa pkaa!"
-
